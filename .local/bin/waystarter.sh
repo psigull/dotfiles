884ae11d3a2343d66a25ab7ed593ea85ada969cf
psigull <git@psigull.dev>
2025-10-15 22:54:46 -0600

    waybar hot reload

---
+++ .local/bin/waystarter.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CONFIGS="$HOME/.config/waybar/config.jsonc $HOME/.config/waybar/style.css"
+
+trap "killall waybar" EXIT
+
+killall waybar
+while true; do
+	waybar &
+	inotifywait -e create,modify $CONFIGS
+	killall waybar
+done
