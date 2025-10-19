d787565796fae17f0f1d4cbc313ec8ab2191138c
psigull <git@psigull.dev>
2025-10-19 11:39:30 -0600

    improve waystarter script

--- .local/bin/waystarter.sh
+++ .local/bin/waystarter.sh
@@ -2,11 +2,33 @@
 
 CONFIGS="$HOME/.config/waybar/config.jsonc $HOME/.config/waybar/style.css"
 
-trap "killall waybar" EXIT
+PID_WAYBAR=""
+PID_NOTIFY=""
 
+cleanup() {
+	if [[ -n "$PID_NOTIFY" ]]; then
+		kill $PID_NOTIFY
+		wait $PID_NOTIFY
+	fi
+	
+	if [[ -n "$PID_WAYBAR" ]]; then
+		kill $PID_WAYBAR
+		wait $PID_WAYBAR
+	fi
+}
+
+trap cleanup EXIT
 killall waybar
+
 while true; do
 	waybar &
-	inotifywait -e create,modify $CONFIGS || continue
-	killall waybar
+	PID_WAYBAR=$!
+
+	inotifywait -e create,modify $CONFIGS &
+	PID_NOTIFY=$!
+
+	wait -n
+	cleanup
+	
+	sleep 1
 done
