ed2d17515c8512a8eedd7d045cb4684a967714a5
psigull <git@psigull.dev>
2025-12-03 02:50:03 -0700

    almost resembling an editor now!

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,5 @@
 local vim = vim
-vim.cmd('startinsert')
+vim.cmd('startinsert') -- 9/10 dentists hate this
 
 vim.opt.number = true
 vim.opt.wrap = true
@@ -29,58 +29,65 @@ local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
 -- swap paste before/after cursor
-map('n', 'p', 'P', opts)
-map('n', 'P', 'p', opts)
+map('n', 'p', 'P', opts) 
+map('n', 'P', 'p', opts) 
 
 -- inSert
-map('n', 's', 'i', opts)
+map('n', 's', 'i', opts) 
 
--- save and quit
-map('n', '<C-s>', ':w<CR>', opts)
-map('v', '<C-s>', '<C-C>:w<CR>', opts)
-map('i', '<C-s>', '<Esc>:w<CR>a', opts)
+-- save
+map('n', '<C-s>', ':w<CR>', opts) 
+map('v', '<C-s>', '<C-C>:w<CR>', opts) 
+map('i', '<C-s>', '<Esc>:w<CR>a', opts) 
 
-map('n', '<C-q>', ':q<CR>', opts)
-map({'v','i'}, '<C-q>', '<Esc>:q<CR>', opts)
+-- close tab/buffer
+map('n', '<C-q>', ':conf bd<CR>', opts) 
+map({'v','i'}, '<C-q>', '<Esc>:conf bd<CR>', opts) 
 
-map('n', '<C-S-q>', ':qa<CR>', opts)
-map({'v','i'}, '<C-S-q>', '<Esc>:qa<CR>', opts)
+-- close nvim
+map('n', '<C-S-q>', ':conf qa<CR>', opts) 
+map({'v','i'}, '<C-S-q>', '<Esc>:conf qa<CR>', opts) 
 
 -- copy/cut/paste
-map('n', '<C-c>', 'yy', oCpts)
-map('v', '<C-c>', 'ya', opts)
-map('i', '<C-c>', '<Esc>yya', opts)
+map('n', '<C-c>', 'yy', opts) 
+map('v', '<C-c>', 'ya', opts) 
+map('i', '<C-c>', '<Esc>yya', opts) 
 
-map('n', '<C-x>', 'dd', opts)
-map('v', '<C-x>', 'da', opts)
-map('i', '<C-x>', '<Esc>dda', opts)
+map('n', '<C-x>', 'dd', opts) 
+map('v', '<C-x>', 'da', opts) 
+map('i', '<C-x>', '<Esc>dda', opts) 
 
 map({'n','v'}, '<C-v>', 'P=`]', opts) -- reindent
-map('i', '<C-v>', '<Esc>p=`]a', opts)
+map('i', '<C-v>', '<Esc>p=`]a', opts) 
+-- TODO: function to trim whitespace, reindent, and place cursor at end
 
 -- undo/redo
-map('n', '<C-z>', 'u', opts)
-map('v', '<C-z>', '<C-C>u', opts)
-map('i', '<C-z>', '<Esc>u', opts)
+map('n', '<C-z>', 'u', opts) 
+map('v', '<C-z>', '<C-C>u', opts) 
+map('i', '<C-z>', '<Esc>u', opts) 
 
-map('n', '<C-S-z>', '<C-r>', opts)
-map('v', '<C-S-z>', '<C-r>gv', opts)
-map('i', '<C-S-z>', '<C-o><C-r>', opts)
+map('n', '<C-S-z>', '<C-r>', opts) 
+map('v', '<C-S-z>', '<C-r>gv', opts) 
+map('i', '<C-S-z>', '<C-o><C-r>', opts) 
 
--- tabs
-map({'n','v','i'}, '<C-t>', '<Esc>:tabnew<CR>i', opts)
-map({'n','v','i'}, '<C-S-t>', '<Esc>:vnew<CR>i', opts)
-map({'n','v','i'}, '<C-Tab>', '<Esc>:tabnext<CR>i', opts)
-map({'n','v','i'}, '<C-S-Tab>', '<Esc>:tabprev<CR>i', opts)
+-- tab management
+map({'n','v','i'}, '<C-t>', '<Esc>:tabnew<CR>', opts) 
+map({'n','v','i'}, '<C-S-t>', '<Esc>:vnew<CR>', opts) -- split
+-- overwritten below if plugins are available:
+map({'n','v','i'}, '<C-Tab>', '<Esc>:tabnext<CR>i', opts) 
+map({'n','v','i'}, '<C-S-Tab>', '<Esc>:tabprev<CR>i', opts) 
 
--- black hole delete
+-- black hole delete to void register, not clipboard
 map({'n','v'}, '<Del>', '"_x', opts)
-map({'n','v','i'}, '<S-Del>', '<Esc>"_dd', opts)
+map({'n','v','i'}, '<S-Del>', '<Esc>"_dd', opts) -- whole line
 
 -- down arrow creates new line if there isn't one
-local expropts = vim.tbl_extend("force", opts, { expr = true })
-map('n', '<Down>', function() return (vim.fn.line('.') == vim.fn.line('$') and vim.fn.getline('$') ~= '') and 'o' or 'j' end, expropts)
-map('i', '<Down>', function() return (vim.fn.line('.') == vim.fn.line('$') and vim.fn.getline('$') ~= '') and '<End><CR>' or '<C-O>j' end, expropts)
+local exprOpts = vim.tbl_extend("force", opts, { expr = true })
+map('n', '<Down>', function() return (vim.fn.line('.') == vim.fn.line('$') and vim.fn.getline('$') ~= '') and 'o' or 'j' end, exprOpts )
+map('i', '<Down>', function() return (vim.fn.line('.') == vim.fn.line('$') and vim.fn.getline('$') ~= '') and '<End><CR>' or '<C-O>j' end, exprOpts )
+
+-- open terminal to active buffer cwd
+map({'n','v','i'}, '<F2>', function() vim.fn.system(string.format('kitty --detach --directory %s', vim.fn.expand('%:p:h'))) end, opts) 
 
 
 -- colour scheme
@@ -96,11 +103,17 @@ if ok then
 	minideps.setup({})
 	local add = minideps.add
 
+	-- file explorer
+	add({source='mikavilpas/yazi.nvim', depends={'nvim-lua/plenary.nvim'}})
+	map({'n','v','i'}, '<C-e>', function() require('yazi').yazi() end, opts) 
+
+	-- statusbar
 	vim.opt.showmode = false
 	vim.opt.cmdheight = 0
 	add({source='nvim-lualine/lualine.nvim', depends={ 'nvim-tree/nvim-web-devicons' }})
 	require('lualine').setup()
 
+	-- multi caret editing
 	vim.g.VM_maps = {
 		["Find Under"] = "<C-d>",
 		["Find Subword Under"] = "<C-d>",
@@ -109,16 +122,35 @@ if ok then
 	}
 	vim.g.VM_mouse_mappings = 1
 	add({source='mg979/vim-visual-multi'})
-	
-	add({source='LukasPietzschmann/telescope-tabs', checkout='vim_ui_select'})
-	map({'n','v','i'}, "<C-`>", function() require('telescope-tabs').list_tabs() end, opts)
 
-	add({source='RRethy/vim-illuminate'})
-	
+	-- quote, bracket, parenthesis pairs
 	add({source='windwp/nvim-autopairs'})
 	require('nvim-autopairs').setup({})
 
+	-- comment selection w/ 'gc' in normal
 	add({source='numToStr/Comment.nvim'})
 	require('Comment').setup({})
+
+	-- snazzy reorderable tabs
+	add({source='romgrk/barbar.nvim'})
+	require('barbar').setup({auto_hide=true})
+	map({'n','v','i'}, '<C-Tab>', '<Esc>:BufferNext<CR>i', opts) 
+	map({'n','v','i'}, '<C-S-Tab>', '<Esc>:BufferPrevious<CR>i', opts) 
+
+	-- multipurpose fuzzy search popup
+	add({source='nvim-telescope/telescope.nvim', depends={'nvim-lua/plenary.nvim'}})
+	require('telescope').setup({defaults={preview=false}})
+	map('n', '<leader><space>', require('telescope.builtin').buffers, opts) 
+	map('n', '<leader>?', require('telescope.builtin').oldfiles, opts) 
+	map('n', '<leader>/', require('telescope.builtin').current_buffer_fuzzy_find, opts) 
+	-- TODO: telescope git_files, help_tags
+	-- TODO: difference between find_files, grep_string, live_grep?
+	-- TODO: function to find in files, but specify a directory/filter first!
+	
+	-- fuzzy search open tabs
+	add({source='LukasPietzschmann/telescope-tabs'})
+	map({'n','v','i'}, "<C-`>", function() require('telescope-tabs').list_tabs() end, opts)
+
+
 end
 
