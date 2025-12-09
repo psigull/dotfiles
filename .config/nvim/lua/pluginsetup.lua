f80a32cdc88e0224a55f9871f86bf4a0254288d2
psigull <git@psigull.dev>
2025-12-08 21:52:01 -0700

    open dashboard on directory

--- .config/nvim/lua/pluginsetup.lua
+++ .config/nvim/lua/pluginsetup.lua
@@ -75,6 +75,15 @@ if ok then
 		vim.cmd('Dashboard')
 	end})
 
+	vim.api.nvim_create_autocmd('VimEnter', {
+		callback = function ()
+			local arg = vim.fn.argv(0)
+			if arg ~= '' and vim.fn.isdirectory(arg) == 1 then
+				vim.cmd('Dashboard')
+			end
+		end
+	})
+
 	map({'n','v','i'}, '<C-t>', '<Esc>:enew<CR><Esc>:Dashboard<CR>', opts)
 
 	-- file explorer
