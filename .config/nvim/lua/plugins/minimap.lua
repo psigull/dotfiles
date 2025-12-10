49daa5f7318305a2b359d770e9412a89474563b0
psigull <git@psigull.dev>
2025-12-10 03:36:30 -0700

    cleanup

--- .config/nvim/lua/plugins/minimap.lua
+++
@@ -1,7 +0,0 @@
-return {
-	source = 'Isrothy/neominimap.nvim',
-	config = function()
-		df.now(function() require('neominimap.api').disable() end) -- start closed
-		df.map('n', '<C-m>', ':Neominimap Toggle<CR>', df.ko)
-	end
-}
