48d6feec4a41c715b2501efa481c39507cb8b67c
psigull <git@psigull.dev>
2026-05-06 03:38:46 -0600

    fix vram stats

--- .local/bin/stats.sh
+++ .local/bin/stats.sh
@@ -1,11 +1,14 @@
-#!/bin/sh
+#!/bin/zsh
 
 usg_cpu=$[100 - $(vmstat 1 2 | tail -1 | awk '{print $15}')]
 usg_gpu=$(cat /sys/class/drm/card1/device/gpu_busy_percent)
 
 mem_phys=$(free | grep Mem | awk '{printf("%d\n", $3/$2 * 100)}')
 mem_swap=$(free | grep Swap | awk '{printf("%d\n", $3/$2 * 100)}')
-mem_gpu=$(cat /sys/class/drm/card1/device/mem_busy_percent)
+
+mg_usg=$(cat /sys/class/drm/card1/device/mem_info_vram_used)
+mg_max=$(cat /sys/class/drm/card1/device/mem_info_vram_total)
+mem_gpu=$(awk "BEGIN {printf \"%d\", $mg_usg / $mg_max * 100}")
 
 tmp_cpu=$(sensors | grep 'Tctl:' | awk '{print int($2)}')
 tmp_gpu=$(sensors | grep 'junction:' | awk '{printf("%d\n", $2)}')
@@ -15,4 +18,3 @@ mem="$mem_phys/$mem_swap/$mem_gpu"
 tmp="$tmp_cpu/$tmp_gpu"
 
 echo " $usg   $mem   $tmp"
-
